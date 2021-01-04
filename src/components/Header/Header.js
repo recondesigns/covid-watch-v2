8a750bdbb725f4819e4ef7e540b3d961c6a0ea48
Michael Stedman <michaelstedman81@gmail.com>
2021-01-04 10:59:01 -0800

    Updated Header Component
    Added basic content and styling.

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,14 +1,57 @@
 import React from 'react'
 import styled from 'styled-components'
+import menuIcon from '../../assets/icons/menu-icon.svg'
+import userIcon from '../../assets/icons/user-icon.svg'
+
 
 const HeaderContainer = styled.div`
-    border: 1px dotted lightcoral;
+    box-sizing: border-box;
+    width: 100%;
+    height: 56px;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    /* border: 1px dotted lightcoral; */
+
+    & > .menu-icon-container {
+        margin: 0px 16px 0px 16px;
+        width: 40px;
+        height: 40px;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+
+        & > img {
+            width: 100%;
+            height: 100%;
+        }
+    }
+
+    & > .user-icon-container {
+        margin: 0px 16px 0px 16px;
+        width: 40px;
+        height: 40px;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+
+        & > img {
+            width: 100%;
+            height: 100%;
+        }
+    }
 `
 
 export default function Header() {
     return (
         <HeaderContainer>
-            <p>Header</p>
+            <div className={'menu-icon-container'}>
+                <img src={menuIcon} alt={'Open menu'} />
+            </div>
+
+            <div className={'user-icon-container'}>
+                <img src={userIcon} alt={'User profile'} />
+            </div>
         </HeaderContainer>
     )
 }
